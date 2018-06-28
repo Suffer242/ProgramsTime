abd2caa2d226df8cffe044da216681a01b02a4ae
timur <timur@silver.rain>
2018-06-28 13:44:55 +0300

    Добавьте файлы проекта.

---
+++ ProgramsTime/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTime
+{
+
+    public static class helpers
+    {
+        public static IEnumerable<DateTime> Dates(DateTime StartDate, DateTime EndDate)
+        {
+            for (DateTime i = StartDate; i <= EndDate; i=i.AddDays(1))
+            {
+                yield return i;
+            }
+        }
+
+        public static IEnumerable<DateTime> Dates2(DateTime StartDate, DateTime EndDate)
+        {
+            for (; StartDate <= EndDate; StartDate = StartDate.AddDays(1))
+            {
+                yield return StartDate;
+            }
+        }
+
+        public static IEnumerable<DateTime> Dates3(DateTime StartDate, DateTime EndDate)
+        {
+            while (StartDate <= EndDate)
+            {
+                yield return StartDate;
+                StartDate = StartDate.AddDays(1);
+            }
+        }
+
+        public static IEnumerable<DateTime> Dates4(DateTime StartDate, DateTime EndDate)
+        {
+            while (StartDate <= EndDate)
+            {
+                yield return StartDate;
+                StartDate = StartDate.AddDays(1);
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void  WeekStat(DateTime sdate)
+    {
+
+        TimeSpan total_info = new TimeSpan();
+        TimeSpan total_reclam = new TimeSpan();
+        var cnt = 0;
+
+        for (DateTime dt = sdate.AddDays(0); dt < sdate.AddDays(7); dt = dt.AddDays(1))
+        {
+
+
+            var l = new List<String>();
+            var items = Common.GetProgsByDay(dt).ToList();
+
+            TimeSpan info = new TimeSpan();
+            TimeSpan reclam = new TimeSpan();
+
+            foreach (var itemgroup in items.GroupBy(f => f.ProgHour)) // выводи почасу
+            {
+                l.Add(itemgroup.Key + " ----");
+              //  Console.WriteLine(itemgroup.Key + " ----");
+
+                TimeSpan hour_info = new TimeSpan();
+                TimeSpan hour_reclam = new TimeSpan();
+
+                foreach (var item in itemgroup.ToList())
+                {
+                 //   Console.WriteLine(item.ToString(items));
+
+                    if (item.Prog.Kind == "Информационно-аналитическое") info += item.GetProgDuration(items);
+                    if (item.Prog.Kind == "Реклама") reclam += item.GetProgDuration(items);
+
+                    if (item.Prog.Kind == "Информационно-аналитическое") hour_info += item.GetProgDuration(items);
+                    if (item.Prog.Kind == "Реклама") hour_reclam += item.GetProgDuration(items);
+
+                    l.Add(item.ToString(items));
+                }
+
+                l.Add("--> Информационно-аналитическое: " + hour_info);
+                l.Add("--> Реклама: " + hour_reclam);
+                l.Add("");
+
+            }
+
+            total_info += info;
+            total_reclam += reclam;
+
+            l.Add(""); l.Add("");
+            l.Add("----> Информационно-аналитическое: " + info);
+            l.Add("----> Реклама: " + reclam);
+
+            File.WriteAllLines(dt.ToShortDateString() + " log.txt", l);
+            cnt++;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(sdate.ToShortDateString() + " - " + sdate.AddDays(6).ToShortDateString());
+        Console.WriteLine("total_info: " + total_info.TotalMinutes);
+        Console.WriteLine("total_reclam: " + total_reclam.TotalMinutes);
+        Console.WriteLine("procent: " + total_info.TotalMinutes / (cnt * 24 * 60 - total_reclam.TotalMinutes) * 100);
+
+    }
+
+        static void Main(string[] args)
+        {
+
+            //foreach ( var d in helpers.Dates4(new DateTime(2018, 2, 12), new DateTime(2018, 2, 18)) )
+            //{
+            //    Console.WriteLine(d);
+            //}
+
+            //Console.ReadKey();
+            //return;
+
+
+            var sdate = new DateTime(2018, 04, 16);
+            for (int i = 0; i < 6+4; i++, sdate = sdate.AddDays(7))
+            {
+                WeekStat(sdate);
+              
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ProgramsTime/radioprogram.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static ProgramsTime.Common;
+
+namespace ProgramsTime
+{
+
+    class Common
+    {
+
+        public class DigitonFile
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public String Alias { get; set; }
+            public String File { get; set; }
+
+            public int DisconnectCount { get; set; }
+            public int DisconnectLevel { get; set; }
+
+            public FileTypes Type { get; set; } = FileTypes.Audio;
+
+            public bool isRed()
+            {
+                return Procent > 110 && (DisconnectCount - DisconnectLevel) >= 3;
+            }
+
+            public int Procent
+            {
+                get
+                {
+                    return DisconnectLevel > 0 ? (int)Math.Round((float)DisconnectCount / DisconnectLevel * 100.0) : 0;
+                }
+            }
+
+            public TimeSpan Duration
+            {
+                get
+                {
+                    return EndDate - StartDate;
+                }
+            }
+
+            public override string ToString()
+            {
+                return (EndDate - StartDate).ToString() + "\t" + StartDate + "\t" + EndDate + "\t" + Alias + "\t" + File;
+            }
+        }
+
+        public enum FileTypes { Audio = 0, Reclam = 1, News = 2, Program = 3 };
+
+
+        class dcomparator : IEqualityComparer<Match>
+        {
+            public bool Equals(Match x, Match y)
+            {
+                return x.Groups[2].Value == y.Groups[2].Value && x.Groups[3].Value == y.Groups[3].Value && x.Groups[4].Value == y.Groups[4].Value; 
+
+            }
+
+            public int GetHashCode(Match obj)
+            {
+                return obj.Groups[4].Value.GetHashCode();
+                
+            }
+        }
+        public static IEnumerable<DigitonFile> GetDayLog(DateTime dt)
+        {
+            String DigitonDir = @"\\digiton001\SoundFiles\Плейлистывещания\";
+            //DigitonDir = @"c:\newweb\dpm\";
+
+            var regstart = new Regex(@"<\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t.*", RegexOptions.Compiled | RegexOptions.Multiline);
+
+            var regend = new Regex(@">\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t.*", RegexOptions.Compiled | RegexOptions.Multiline);
+
+            String CurDigiton = null;
+            try
+            {
+                CurDigiton = System.IO.File.ReadAllText(DigitonDir + $"{dt.Year:d4}{dt.Month:d2}{dt.Day:d2}" + ".dpm", Encoding.GetEncoding(1251));
+            }
+            catch
+            {
+                DigitonDir = @"c:\newweb\dpm\";
+                CurDigiton = System.IO.File.ReadAllText(DigitonDir + $"{dt.Year:d4}{dt.Month:d2}{dt.Day:d2}" + ".dpm", Encoding.GetEncoding(1251));
+            }
+
+            // var matchstart = regstart.Matches(CurDigiton).Cast<Match>().Distinct( new dcomparator());
+            // var matchend = regend.Matches(CurDigiton).Cast<Match>().Distinct(new dcomparator());
+
+            var matchstart = regstart.Matches(CurDigiton).Cast<Match>().GroupBy(f => f.Groups[2].Value + f.Groups[4].Value).Select(f => f.First());
+            var matchend = regend.Matches(CurDigiton).Cast<Match>().GroupBy(f => f.Groups[2].Value + f.Groups[4].Value).Select(f => f.Last());
+
+            return from start in matchstart
+                   join end in matchend
+                   on start.Groups[2].Value+ start.Groups[4].Value equals end.Groups[2].Value+ end.Groups[4].Value
+                   select new DigitonFile
+                   {
+                       StartDate = DateTime.Parse(start.Groups[1].Value),
+                       EndDate = DateTime.Parse(end.Groups[1].Value),
+                       File = start.Groups[4].Value,
+                       Alias = start.Groups[3].Value,
+
+                       Type = start.Groups[3].Value == "РЕКЛАМА" ? FileTypes.Reclam
+                       : start.Groups[4].Value.ToLower().Contains("новости") ? FileTypes.News
+                       : start.Groups[3].Value.ToLower() == "back-jingles programs" ? FileTypes.Program : FileTypes.Audio
+                   };
+        }
+
+        /*
+        public static Dictionary<int, List<Tuple<DateTime, String, String>>> GetDayLog(DateTime dt)
+        {
+            String DigitonDir = @"\\digiton001\SoundFiles\Плейлистывещания\";
+            var reg = new Regex(@"<\t(.*?)\t.*?\t(.*?)\t(.*?)\t(.*?)\t.*", RegexOptions.Compiled | RegexOptions.Multiline);
+
+            var CurDigiton = System.IO.File.ReadAllText(DigitonDir + $"{dt.Year:d4}{dt.Month:d2}{dt.Day:d2}" + ".dpm", Encoding.GetEncoding(1251));
+
+            MatchCollection m = reg.Matches(CurDigiton);
+
+            return m.Cast<Match>().Select(f => Tuple.Create(DateTime.Parse(f.Groups[1].Value), f.Groups[2].Value, f.Groups[3].Value))
+                 .GroupBy(f => f.Item1.Hour).ToDictionary(f => f.Key, f => f.ToList());
+
+        }
+        */
+
+        public static IEnumerable<radioprogram> GetProgs()
+        {
+
+            return System.IO.File.ReadAllLines("progs.txt", Encoding.GetEncoding(1251)).Select(f => f.Split('\t')).Select(f => new radioprogram
+            {
+                Name = f[0],
+                Duration = f[1] == "1 час" ? 60 : 3,
+                OtbList = new String[] { f[2] },
+                Kind = f[3]
+            });
+
+            //var text = System.IO.File.ReadAllText("progs.txt", Encoding.GetEncoding(1251));
+            //var reg = new Regex(@"(.*?) \((.*?)\)(.*?)\r\n\r\n", RegexOptions.Compiled | RegexOptions.Singleline);
+
+            //var m = reg.Matches(text);
+
+            //var r = m.Cast<Match>().Select(f => new radioprogram
+            //{
+            //    Name = f.Groups[1].Value,
+            //    Duration = Int32.Parse(f.Groups[2].Value),
+            //    OtbList = String.IsNullOrWhiteSpace(f.Groups[3].Value) ? null : f.Groups[3].Value.Trim().Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries),
+                
+               
+
+            //});
+
+            
+
+            //return r;
+
+        }
+
+        
+        public static IEnumerable<ProgEnter> GetProgsByDay(DateTime dt)
+        {
+            var log = GetDayLog(dt);
+            var progs = GetProgs();
+
+            foreach (var hour in log.GroupBy(f=>f.StartDate.Hour))
+            {
+                var h = hour;
+
+                foreach (var p in progs)
+                {
+                    foreach (var r in p.HasInHour(h.ToList()))
+                     yield return r;
+
+                   //if (pe != null) yield return pe;
+                }
+            }
+        }
+
+    }
+    class radioprogram
+    {
+        public String Name { get; set; }
+        public int Duration { get; set; }
+        public IEnumerable<String> OtbList { get; set; }
+
+        public String Kind { get; set; }
+        public bool IsHourProg { get { return Duration == 60; } }
+
+        public override string ToString()
+        {
+            return Name + " : " + Duration;
+        }
+
+
+        public IEnumerable<ProgEnter> HasInHour(List<DigitonFile> hourlog)
+        {
+            if (OtbList == null) yield break;
+
+
+            foreach (var item in hourlog)
+            {
+                if (!(Duration == 60 && item.Alias == "РЕКЛАМА"))
+                    foreach (var otb in OtbList)
+                        if ( (item.File+" "+item.Alias).Contains(otb))
+                        {
+                           var pe = new ProgEnter { Prog=this,ProgDate=item.StartDate ,ProgHour=item.StartDate.Hour,StartMinute=item.StartDate, EndMinute = item.EndDate, jingle = otb, File=item.File};
+                            var ishour = Duration == 60;
+                             yield return pe;
+
+                           if (ishour) yield break;
+                        } 
+                
+            }
+
+        }
+    }
+
+    class ProgEnter
+    {
+        public radioprogram Prog { set; get; }
+        public DateTime ProgDate { get; set; }
+        public int ProgHour { get; set; }
+        public DateTime StartMinute { get; set; }
+
+        public DateTime EndMinute { get; set; }
+
+        public String jingle { get; set; }
+
+        public String File { get; set; }
+
+        public override string ToString()
+        {
+            return Prog.Name + " ("+ Prog.Duration + " мин) : " + ProgDate.ToString() + " - распознаная метка -> " + jingle;
+        }
+
+        public string ToString( IEnumerable<ProgEnter> items)
+        {
+            var d = GetProgDuration(items);
+            return (Prog.Duration == 60 ? "#####ЧАС#####>" : "") + Prog.Name + " (" + d + " мин) : " + ProgDate.ToString() + " - " + Prog.Kind +  " en:" + EndMinute.ToLongTimeString()
+                + " [" + File+"]";
+
+
+            //" - распознаная метка -> " + jingle;
+        }
+
+        public TimeSpan GetProgDuration(IEnumerable<ProgEnter> plist)
+        {
+            if (Prog.Duration < 60) return EndMinute - StartMinute;
+            else
+                return TimeSpan.FromSeconds(Prog.Duration*60 - plist.Where(f => f.ProgHour == ProgHour && !f.Prog.IsHourProg).Sum(f => f.GetProgDuration(plist).TotalSeconds));
+        }
+
+    }
+}
